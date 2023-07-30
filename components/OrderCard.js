3432147538c13bdab230a857a13bb8931bb958a2
alvinaleguiojo <alvin.aleguiojo@gmail.com>
2023-07-31 05:04:50 +0800

    initial commit

---
+++ components/OrderCard.js
@@ -0,0 +1,18 @@
+import styles from "../src/app/TradeCard.module.css";
+
+const TradeCard = ({ order }) => {
+  return (
+    <div className={styles.card}>
+      <h2>{order.symbol}</h2>
+      <strong>Profit:</strong>
+      {order.profit < 0 ? (
+        <p style={{ color: "red" }}>{order.profit}</p>
+      ) : (
+        <p>{order.profit}</p>
+      )}
+      <strong>Lotsize:</strong> {order.lots}
+    </div>
+  );
+};
+
+export default TradeCard;
